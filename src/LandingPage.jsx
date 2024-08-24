0269bf4a44446e9fd098252081e5edf9d8cd80a4
Ryan@Tenxrs <mpenyanaryan@gmail.com>
2024-08-24 17:58:22 +0200

    mobile fixes

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -6,7 +6,7 @@ import orangeTopImage from './assets/topbobble.svg'
 import greenBottomImage from './assets/green.svg'
 const LandingPage = () => {
     return (
-        <div className="relative hero mb-8 bg-hero bg-cover bg-no-repeat bg-center h-48 lg:h-[450px] xl:h-[560px] lg:mb-16">
+        <div className="relative hero mb-8 bg-hero bg-cover bg-no-repeat bg-center h-48 md:h-[300px] lg:h-[450px] xl:h-[560px] lg:mb-16">
             <div className='absolute w-10 lg:w-20 xl:w-full'>
                 <img src={orangeTopImage} alt="" />
             </div>
@@ -14,17 +14,17 @@ const LandingPage = () => {
                 <img src={greenBottomImage} alt="" />
             </div>
             <div className="flex relative flex-col h-full justify-end sm:items-center">
-                <div className='z-10 sm:w-8/12 xl:w-[912px] pb-2  lg:pb-6 '>
+                <div className='z-10 sm:w-8/12 lg:w-7/12 xl:w-[912px] pb-2  lg:pb-6 '>
                     <div className='sm:text-left'> 
                         <h1 className='xl:text-2xl text-white font-medium lg:mb-4'>Waarmee kunnen we je helpen?</h1>
                     </div>
-                    <div className="flex flex-col w-full items-center gap-1 sm:flex-row sm:justify-center lg:gap-5">
-                        <button className="bg-white w-36 md:w-44 md:h-9 lg:w-72 lg:h-12 lg:gap-3 xl:w-72 xl:h-16 xl:text-lg lg:font-semibold lg:p-4 text-[#39468C] items-center gap-1 text-sm p-1 rounded-lg flex"><span><img className='w-4 lg:w-8' src={hammerIcon} alt="button-icon" /></span>Reparatie melden</button>
-                        <button className="bg-white w-36 md:w-44 md:h-9 lg:w-72 lg:h-12 lg:gap-3 xl:w-72 xl:h-16 xl:text-lg lg:font-semibold lg:p-4 text-[#39468C] items-center gap-1 text-sm p-1 rounded-lg flex"><span><img className='w-4 lg:w-8' src={handDepositIcon} alt="button-icon" /></span>Huur betalen</button>
-                        <button className="bg-white w-36 md:w-44 md:h-9 lg:w-72 lg:h-12 lg:gap-3 xl:w-72 xl:h-16 xl:text-lg lg:font-semibold lg:p-4 text-[#39468C] items-center gap-1 text-sm p-1 rounded-lg flex"><span><img className='w-4 lg:w-8' src={circleChatsIcon} alt="button-icon" /></span>Contact</button>
+                    <div className="flex  w-full justify-center gap-1 sm:flex-row sm:justify-center lg:gap-4 xl:gap-6">
+                        <button className="text-[8px] font-bold md:font-semibold w-28 md:text-[16px] bg-white md:w-44 md:h-9 lg:w-72 lg:h-12 lg:gap-3 xl:w-72 xl:h-16 xl:text-lg lg:font-semibold lg:p-4 text-[#39468C] items-center gap-1 text-sm p-1 rounded-lg flex"><span><img className='w-2 md:w-4 lg:w-8' src={hammerIcon} alt="button-icon" /></span>Reparatie melden</button>
+                        <button className="text-[8px] font-bold md:font-semibold w-28 md:text-[16px] bg-white  md:w-44 md:h-9 lg:w-72 lg:h-12 lg:gap-3 xl:w-72 xl:h-16 xl:text-lg lg:font-semibold lg:p-4 text-[#39468C] items-center gap-1 text-sm p-1 rounded-lg flex"><span><img className='w-2 md:w-4 lg:w-8' src={handDepositIcon} alt="button-icon" /></span>Huur betalen</button>
+                        <button className="text-[8px] font-bold md:font-semibold w-28 md:text-[16px] bg-white  md:w-44 md:h-9 lg:w-72 lg:h-12 lg:gap-3 xl:w-72 xl:h-16 xl:text-lg lg:font-semibold lg:p-4 text-[#39468C] items-center gap-1 text-sm p-1 rounded-lg flex"><span><img className='w-2 md:w-4 lg:w-8' src={circleChatsIcon} alt="button-icon" /></span>Contact</button>
                     </div>
                 </div>
-                <div className='absolute w-3/4  self-center'>
+                <div className='absolute w-3/4 xl:w-3/4 lg:w-9/12 self-center'>
                     <img src={waveImage} alt="" />
                 </div>
             </div>
